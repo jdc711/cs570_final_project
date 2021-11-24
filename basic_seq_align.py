e9c9f7ce111f335e0d0239c9fd3e97731b37ae92
Joshua Choe <choejd@usc.edu>
2021-11-24 00:30:59 -0800

    basic align works for test case : X = "hello", Y = "hey"

--- basic_seq_align.py
+++ basic_seq_align.py
@@ -41,6 +41,8 @@
 
 # We will go left to right, computing row by row starting from the bottom row
 
+
+
 def MISMATCH(x, y):
     return 2
 
@@ -52,24 +54,29 @@ def basic_seq_align(X, Y, gap_pen):
     n = len(Y)
     # define 2d list OPT
     OPT = []
-    for i in range(n):
-        col = []
-        OPT.append(col)
-
+    for i in range(m+1):
+        OPT.append([])
+        for j in range(n+1):
+            OPT[i].append(0) 
+    print(OPT)
+    
     # initialize necessary locations of OPT array
     # special note! in OPT grid, we reserve 0th row/col for "" and ""; the 1st row/col for "x1" and "y1"; the 2nd row/col for "x1x2" and "y1y2"
-    for i in range(m):
+    for i in range(0,m+1):
+        print(i)
         OPT[i][0] = i*gap_pen
-    for j in range(n):
+        
+        
+    for j in range(0,n+1):
         OPT[0][j] = j*gap_pen
 
     # compute value of OPT solution BOTTOM UP; optimal value will be found at OPT[m][n] 
     
-    for i in range(1, m):
-        for j in range(1, n):
+    for i in range(1, m+1):
+        for j in range(1, n+1):
             # string is indexed by zerp; to access ith character, subtract i-1
-            OPT[i][j] = min((OPT(i-1, j-1) + MISMATCH(X[i-1], Y[j-1])),     
-                            (OPT(i-1, j) + gap_pen), (OPT(i, j-1) + gap_pen))
+            OPT[i][j] = min((OPT[i-1][j-1] + MISMATCH(X[i-1], Y[j-1])),     
+                            (OPT[i-1][j] + gap_pen), (OPT[i][j-1] + gap_pen))
     
     # Build optimal solution TOP DOWN
     i = m
@@ -81,23 +88,34 @@ def basic_seq_align(X, Y, gap_pen):
 
     
     
-    while (i >= 0 and j >= 0):
+    while (i > 0 and j > 0):
         # compare OPT[i][j] with 1) OPT[i-1][j] (if Xm is mismatched), 2) OPT[i][j-1] (if Yn is mismatched), 3) OPT[i-1][j-1] (if (Xm,Yn) is in our optimal solution
         if OPT[i][j] == OPT[i-1][j] + gap_pen:
             # Xi is mismatched with gap, append gap to end of Y_sol and Xi to end of X_sol
-            X_sol = X_sol + X[i]
-            Y_sol = Y_sol + '_'
+            X_sol = X[i-1] + X_sol
+            Y_sol = '_' + Y_sol
             i = i - 1
+            print("X: ", X_sol)
+            print("Y: ", Y_sol)
         elif OPT[i][j] == OPT[i][j-1] + gap_pen:
             # Yj is mismatched with gap, append gap to end of X_sol and Yj to end of Y_sol
-            Y_sol = Y_sol + Y[j]
-            X_sol = X_sol + '_'
+            Y_sol = Y[j-1] + Y_sol  
+            X_sol = '_' + X_sol 
             j = j - 1
+            print("X: ", X_sol)
+            print("Y: ", Y_sol)
         elif OPT[i][j] == OPT[i-1][j-1] + MISMATCH(X[i-1],Y[j-1]):
             # (Xi,Yj) is in our optimal solution; append Xi to X_sol and Yj to Y_sol
-            X_sol = X_sol + X[i]
-            Y_sol = Y_sol + Y[j]
+            X_sol =  X[i-1]+ X_sol 
+            Y_sol = Y[j-1] + Y_sol 
             i = i - 1
             j = j - 1
+            print("X: ", X_sol)
+            print("Y: ", Y_sol)
+    print("X: ", X_sol)
+    print("Y: ", Y_sol)
         
+if __name__ == "__main__":
+    basic_seq_align("hello", "hey", 3)
+    
         
